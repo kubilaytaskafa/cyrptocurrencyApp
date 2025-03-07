5e1c38f3dec16e54114be476f64a13ad14e77460
kubilay taşkafa <info.kubilay0@gmail.com>
2025-03-08 00:58:17 +0300

    CoinItem.jsx dosyası güncellendi

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CoinItem = ({ coin }) => {
   return (
-    <div className="text-white flex justify-between items-center shadow-lg rounded-lg lg:mx-4 my-8  px-6 py-3 hover:scale-105 transition-all duration-300 cursor-pointer w-full bg-gray-800   ">
+    <div className="text-white flex justify-between items-center shadow-lg rounded-lg lg:mx-4 my-8  px-6 py-3 hover:scale-105 transition-all duration-300 ease-in cursor-pointer w-full bg-gray-800   ">
       <p>{coin.market_cap_rank}</p>
       <div className="flex items-center gap-x-2 w-auto">
         <img src={coin.image} alt="" className="w-10" />
